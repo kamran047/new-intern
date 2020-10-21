d97a77666afdcf4f3055799c7a6181db8e9f1ea1
Kamran <kamran.nazir047@gmail.com>
2020-10-21 12:39:37 +0500

    Authorization Completed

---
+++ Web API's/WebAPI/BusinessLogic/UserLogic.cs
@@ -0,0 +1,28 @@
+﻿using DataLayer;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class UserLogic: IDisposable
+    {
+       Context context =new Context();
+        public User ValidateUser(string username, string password)
+        {
+            {
+                return context.Users.FirstOrDefault(user =>
+                       user.UserName.Equals(username, StringComparison.OrdinalIgnoreCase)
+                       && user.UserPassword == password);
+            }
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
---
+++ Web API's/WebAPI/WebAPI/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebAPI.Startup))]
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Enabling CORS (cross origin resource sharing) for making request using browser from different domains
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                //The Path For generating the Toekn
+                TokenEndpointPath = new PathString("/Login"),
+                //Setting the Token Expired Time (24 hours)
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                //MyAuthorizationServerProvider class will validate the user credentials
+                Provider = new MyAuthorizationServerProvider()
+            };
+            //Token Generations
+            app.UseOAuthAuthorizationServer(options);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+        }
+    }
+}
