276730b26eb5b72b703854015b482351a3ff52ed
Kamran <kamran.nazir047@gmail.com>
2020-12-02 11:45:56 +0500

    Cucumber Testing

---
+++ Web API's/WebAPI/CucumberTesting/DeleteStudentSteps.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace CucumberTesting
+{
+    [Binding]
+    public class DeleteStudentSteps
+    {
+        Client client = new Client();
+        [Given(@"The Student Id")]
+        public void GivenTheStudentId()
+        {
+            Console.WriteLine("");
+        }
+        
+        [When(@"The delete button is pressed")]
+        public async System.Threading.Tasks.Task WhenTheDeleteButtonIsPressedAsync()
+        {
+            await client.GeneralRequestAsync("api/student/86", null, "Delete");
+        }
+        
+        [Then(@"The record should be deleted from the database")]
+        public void ThenTheRecordShouldBeDeletedFromTheDatabase()
+        {
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ Web API's/WebAPI/CucumberTesting/LoginSteps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using TechTalk.SpecFlow;
+
+namespace CucumberTesting
+{
+    [Binding]
+    public class LoginSteps
+    {
+        Client client = new Client();
+
+        [Given(@"The credentials of user")]
+        public void GivenTheCredentialsOfUser()
+        {
+            Console.WriteLine("");
+        }
+        
+        [When(@"the login button is pressed")]
+        public async System.Threading.Tasks.Task WhenTheLoginButtonIsPressedAsync()
+        {
+            var values = new Dictionary<string, string>
+        {
+         { "grant_type", "password" },
+         { "username", "Kamran" },
+         { "password", "Kamran" }
+        };
+            var content = new FormUrlEncodedContent(values);
+            await client.GeneralRequestAsync("Login", content, "Post");
+        }
+        
+        [Then(@"the user should be logged in")]
+        public void ThenTheUserShouldBeLoggedIn()
+        {
+            Console.WriteLine("");
+        }
+    }
+}
